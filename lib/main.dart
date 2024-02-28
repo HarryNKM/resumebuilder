d8a17db13c862cc16ab6754835443c900f9a834c
harryNKM <harrynakum05@gmail.com>
2024-02-28 22:38:59 +0530

    third

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:resumebuilder/screen/home/home_screen.dart';
 import 'package:resumebuilder/utils/routes/routes.dart';
+import 'package:flutter/services.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   SystemChrome.setSystemUIOverlayStyle(
-    SystemUiOverlayStyle(statusBarColor: Colors.blue,systemNavigationBarColor: Colors.blue),
+    const SystemUiOverlayStyle(statusBarColor: Colors.blue,systemNavigationBarColor: Colors.blue),
   );
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp
+  ]);
+  
   runApp(MaterialApp(debugShowCheckedModeBanner: false, routes: app_routes));
 }
